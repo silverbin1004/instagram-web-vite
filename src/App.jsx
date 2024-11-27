8745fcadb84120fdcfafa5c90671d6b87980da69
silverbin1004 <silverbinmin1004@gmail.com>
2024-11-27 16:28:37 +0900

    title text 작성

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>facebook</h1>
+      <h2>Welcome to my facebook!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
